5a81d4e99f0f8f13802e6576ae09c6009bef76be
dXcess <ryan.r.andrews@gmail.com>
2026-01-18 11:20:05 -0500

    Add constants for National Grid integration

---
+++ custom_components/nationalgrid/const.py
@@ -0,0 +1,6 @@
+DOMAIN = "nationalgrid"
+
+TOKEN_URL = "https://myaccount.nationalgrid.com/oauth2/token"
+VERIFY_URL = "https://myaccount.nationalgrid.com/api/amiadapter-cu-uwp-sys"
+
+APIM_SUBSCRIPTION_KEY = "PUT_THE_KEY_YOU_FOUND_HERE"
